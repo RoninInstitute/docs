5a4bbf333b2c39bc6cc1b2a3b95f60e8ce8b8fab
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-04-09 13:11:26 -0500

    add first batch of tracked versions

---
+++ track.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# tracks Reframe Ronin documents versions using Preston [1,2]
+#
+#
+# [1] Elliott M.J., Poelen J.H., Fortes J.A.B. (2020). Toward Reliable Biodiversity Dataset References. Ecological Informatics. https://doi.org/10.1016/j.ecoinf.2020.101132 hash://sha256/136c3c1808bcf463bb04b11622bb2e7b5fba28f5be1fc258c5ea55b3b84f482c
+#
+# [2] Elliott M.J., Poelen, J.H. & Fortes, J.A.B. (2023) Signing data citations enables data verification and citation persistence. Sci Data. https://doi.org/10.1038/s41597-023-02230-y hash://sha256/f849c870565f608899f183ca261365dce9c9f1c5441b1c779e0db49df9c2a19d
+#
+
+echo -e "docx\nodt\npdf\nrtf\ntxt"\
+ | sed "s+^+https://docs.google.com/document/u/0/export?format=+g"\
+ | sed "s+$+\&id=1lyAYpU67imn5ceAjzv4UDGlmqw0F_mpW_oRRqb-C6FI+g"\
+ | xargs preston track
+
