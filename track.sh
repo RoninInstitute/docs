da982969bede76d0111ace593f2d73f3d2e1e6c4
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-12-03 17:10:07 -0600

    include belonging and consensus documents

--- track.sh
+++ track.sh
@@ -8,23 +8,32 @@
 # [2] Elliott M.J., Poelen, J.H. & Fortes, J.A.B. (2023) Signing data citations enables data verification and citation persistence. Sci Data. https://doi.org/10.1038/s41597-023-02230-y hash://sha256/f849c870565f608899f183ca261365dce9c9f1c5441b1c779e0db49df9c2a19d
 #
 
+create_alias() {
+  local alias_name="$1"
+  preston alias | grep docx | head -n1 | preston cat > "${alias_name}.docx"
+  preston alias | grep pdf | head -n1 | preston cat > "${alias_name}.pdf"
+}
 
 preston track https://docs.google.com/document/d/1lyAYpU67imn5ceAjzv4UDGlmqw0F_mpW_oRRqb-C6FI/edit 
 
-preston alias | grep docx | head -n1 | preston cat > reframing-ronin.docx
-preston alias | grep pdf | head -n1 | preston cat > reframing-ronin.pdf
+create_alias "reframing-ronin"
 
 preston track https://docs.google.com/document/d/11ObGQkSDG3-72sgqlqa3rQ5GUvorB1PaU5J5LwQHvAc/edit
 
-preston alias | grep docx | head -n1 | preston cat > bylaws-notes.docx
-preston alias | grep pdf | head -n1 | preston cat > bylaws-notes.pdf
+create_alias "bylaws-notes"
 
 preston track https://docs.google.com/document/d/18iwEiHUZlCjvi_Z-t35vechAHvIM07ltmIrDvkUPxhE/edit
 
-preston alias | grep docx | head -n1 | preston cat > bylaws-investigation.docx
-preston alias | grep pdf | head -n1 | preston cat > bylaws-investigation.pdf
+create_alias "bylaws-investigation"
 
 preston track https://docs.google.com/document/d/1mDvCSI2FZUqAVB0QdWxIw8x4ycBvGRjxduQW7MKrj1A/edit
 
-preston alias | grep docx | head -n1 | preston cat > draft-bylaws.docx
-preston alias | grep pdf | head -n1 | preston cat > draft-bylaws.pdf
+create_alias "draft-bylaws"
+
+preston track https://docs.google.com/document/d/1B-eXZBVeDaXFnD6S7mFpPMK6yeYTfJIlSfhKjHlOpkI/edit
+
+create_alias "belonging-policy"
+
+preston track https://docs.google.com/document/d/18zYXP-kFf904wotx1j_tQ0eh-2MnBKnzCbpn8n12iuU/edit
+
+create_alias "consensus-decision-making-policy"
